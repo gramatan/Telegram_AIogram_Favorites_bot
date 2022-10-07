1317bcb83c48bdc6e5d59d8ac475c712f6189fe5
gramatan <78417563+gramatan@users.noreply.github.com>
2022-10-07 12:47:06 +0700

    some commands

--- bot.py
+++ bot.py
@@ -1,35 +1,26 @@
+import asyncio
 import logging
-from aiogram import Bot, Dispatcher, executor, types
+from aiogram import Bot, Dispatcher, types, executor
 
+logging.basicConfig(level=logging.INFO)
 bot = Bot(token='5777625302:AAE-3GYxBMzr6WkySFPOQQqqan4ZfBV1eSI')
 dp = Dispatcher(bot)
-# logging.basicConfig(level=logging.DEBUG)
-logging.basicConfig(level=logging.INFO)
 
 
-@dp.message_handler(commands="dice")
-async def cmd_dice(message: types.Message):
-    await message.answer_dice(emoji="🎲")
-
-
-@dp.message_handler(commands='start')
-async def cmd_test1(message: types.message):
-    await message.reply('start')
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    await message.reply('Hello World')
 
 
 @dp.message_handler(commands='test1')
-async def cmd_test1(message: types.message):
-    await message.reply('test 1')
+async def cmd_test1(message: types.Message):
+    await message.reply("Test 1")
 
 
-@dp.message_handler(commands="answer")
-async def cmd_answer(message: types.Message):
-    await message.answer("Это простой ответ")
-
-
-@dp.message_handler(commands="reply")
-async def cmd_reply(message: types.Message):
-    await message.reply('Это ответ с "ответом"')
+@dp.message_handler(commands='dice')
+async def send_dice(message: types.Message):
+    await message.answer_dice(emoji='🎲')
 
 if __name__ == '__main__':
+    # Запуск бота
     executor.start_polling(dp, skip_updates=True)
