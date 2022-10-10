abbad1d15737b4c2dde09faaed374ae6eba4ec0b
gramatan <78417563+gramatan@users.noreply.github.com>
2022-10-10 17:39:35 +0700

    it's alive! forward options added

--- bot.py
+++ bot.py
@@ -1,26 +1,32 @@
 import asyncio
 import logging
+
 from aiogram import Bot, Dispatcher, types, executor
 
-logging.basicConfig(level=logging.INFO)
+# logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 bot = Bot(token='5777625302:AAE-3GYxBMzr6WkySFPOQQqqan4ZfBV1eSI')
 dp = Dispatcher(bot)
 
 
+@dp.message_handler(lambda message: message.text and '#notes' in message.text.lower())
+async def send_welcome(message: types.Message):
+    await message.forward(chat_id=-886904261)
+
+
+@dp.message_handler(lambda message: message.text and '#saved' in message.text.lower())
+async def send_welcome(message: types.Message):
+    await message.forward(chat_id=-708423371)
+
+
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
-    await message.reply('Hello World')
-
-
-@dp.message_handler(commands='test1')
-async def cmd_test1(message: types.Message):
-    await message.reply("Test 1")
-
+    await message.reply('Just leave.')
 
 @dp.message_handler(commands='dice')
 async def send_dice(message: types.Message):
     await message.answer_dice(emoji='🎲')
 
+
 if __name__ == '__main__':
-    # Запуск бота
     executor.start_polling(dp, skip_updates=True)
